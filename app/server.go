d49c28c8901b2c5ca78cb61086b590cced420a51
fwznbg <fnaabigh@gmail.com>
2024-03-10 18:37:01 +0700

    concurrent connections

--- app/server.go
+++ app/server.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 )
 
-type RequestHeader struct {
+type Request struct {
 	Method  string
 	Path    string
 	Version string
@@ -23,6 +24,74 @@ const (
 	TextType           = "text/plain"
 	ContentLength      = "Content-Length"
 )
+
+func getStatusText(status int) string {
+	switch status {
+	case 200:
+		return OK_RESPONSE
+	case 404:
+		return NOT_FOUND_RESPONSE
+	default:
+		return "INVALID STATUS"
+	}
+}
+
+func (req *Request) buildResponse(status int, body string) string {
+	response := req.Version + " " + getStatusText(status) + SEPARATOR + SEPARATOR
+	if body != "" {
+		response += ContentType + " " + TextType + SEPARATOR
+		response += ContentLength + " " + fmt.Sprint(len(body)) + SEPARATOR
+		response += SEPARATOR
+		response += body
+	}
+	return response
+}
+
+func parseRequest(conn net.Conn) (Request, error) {
+	var err error
+	scanner := bufio.NewScanner(conn)
+	req := Request{
+		Headers: map[string]string{},
+	}
+	if scanner.Scan() {
+		header := strings.Fields(scanner.Text())
+		req.Method = header[0]
+		req.Path = header[1]
+		req.Version = header[2]
+	} else {
+		err = errors.New("Error parsing request header")
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		header := strings.Split(line, ": ")
+		if len(line) != 0 {
+			req.Headers[header[0]] = header[1]
+		} else {
+			break
+		}
+	}
+
+	return req, err
+}
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	req, err := parseRequest(conn)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if req.Path == "/" {
+		conn.Write([]byte(fmt.Sprintf("%s %s", req.Version, OK_RESPONSE+SEPARATOR+SEPARATOR)))
+	} else if strings.Contains(req.Path, "/echo") {
+		conn.Write([]byte(req.buildResponse(200, strings.Trim(req.Path, "/echo/"))))
+	} else if req.Path == "/user-agent" {
+		conn.Write([]byte(req.buildResponse(200, req.Headers["User-Agent"])))
+	} else {
+		conn.Write([]byte(fmt.Sprintf("%s %s", req.Version, NOT_FOUND_RESPONSE+SEPARATOR+SEPARATOR)))
+	}
+}
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -39,57 +108,6 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-
-		reader := bufio.NewReader(conn)
-		header, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error parsing request header: ", err.Error())
-			os.Exit(1)
-		}
-
-		headerArr := strings.Fields(header)
-		reqHeader := &RequestHeader{
-			Method:  headerArr[0],
-			Path:    headerArr[1],
-			Version: headerArr[2],
-			Headers: map[string]string{},
-		}
-
-		for header, err := reader.ReadString('\n'); err == nil; {
-			h := strings.Split(header, ": ")
-			if len(h) > 1 {
-				reqHeader.Headers[h[0]] = strings.TrimSuffix(h[1], SEPARATOR)
-				header, err = reader.ReadString('\n')
-			} else {
-				break
-			}
-		}
-
-		if reqHeader.Path == "/" {
-			conn.Write([]byte(fmt.Sprintf("%s %s", reqHeader.Version, OK_RESPONSE+SEPARATOR+SEPARATOR)))
-		} else if strings.Contains(reqHeader.Path, "/echo") {
-			reqContent := reqHeader.Path[6:]
-			resHeader := reqHeader.Version + " " + OK_RESPONSE + SEPARATOR
-			resHeader += ContentType + ": " + TextType + SEPARATOR
-			resHeader += ContentLength + ": " + fmt.Sprint(len(reqContent)) + SEPARATOR
-			resHeader += SEPARATOR
-			resBody := reqContent
-			conn.Write([]byte(resHeader + resBody))
-		} else if reqHeader.Path == "/user-agent" {
-			reqContent := reqHeader.Headers["User-Agent"]
-			resHeader := reqHeader.Version + " " + OK_RESPONSE + SEPARATOR
-			resHeader += ContentType + ": " + TextType + SEPARATOR
-			resHeader += ContentLength + ": " + fmt.Sprint(len(reqContent)) + SEPARATOR
-			resHeader += SEPARATOR
-			resBody := reqContent
-			conn.Write([]byte(resHeader + resBody))
-		} else {
-			conn.Write([]byte(fmt.Sprintf("%s %s", reqHeader.Version, NOT_FOUND_RESPONSE+SEPARATOR+SEPARATOR)))
-		}
-
-		err = conn.Close()
-		if err != nil {
-			fmt.Println("Error closing tcp connection: ", err.Error())
-		}
+		go handleConnection(conn)
 	}
 }
