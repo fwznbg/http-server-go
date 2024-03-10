e80a4b92e717cc78db4598ec2b096d6c75a9715e
fwznbg <fnaabigh@gmail.com>
2024-03-10 14:42:34 +0700

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
+)
+
+type RequestHeader struct {
+	Method  string
+	Path    string
+	Version string
+}
+
+const (
+	OK_RESPONSE        = "200 OK\r\n\r\n"
+	NOT_FOUND_RESPONSE = "404 NOT FOUND\r\n\r\n"
 )
 
 func main() {
@@ -15,16 +28,31 @@ func main() {
 		os.Exit(1)
 	}
 
-  conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-  header := "HTTP/1.1 200 OK\r\n\r\n"
-  response := []byte(header)
-  _, err = conn.Write(response)
-  if err != nil {
-    fmt.Println("Error sending data from connection: ", err.Error())
-  }
+		header, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error parsing request header: ", err.Error())
+			os.Exit(1)
+		}
+
+		headerArr := strings.Fields(header)
+		reqHeader := &RequestHeader{
+			Method:  headerArr[0],
+			Path:    headerArr[1],
+			Version: headerArr[2],
+		}
+
+		if reqHeader.Path == "/" {
+			conn.Write([]byte(fmt.Sprintf("%s %s", reqHeader.Version, OK_RESPONSE)))
+		} else {
+			conn.Write([]byte(fmt.Sprintf("%s %s", reqHeader.Version, NOT_FOUND_RESPONSE)))
+		}
+
+	}
 }
